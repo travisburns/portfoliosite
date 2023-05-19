4bf532483833d70e200f534c12cb87fe7ba8227a
Travis <travisjb0@gmail.com>
2023-05-19 14:42:17 -0700

    Added myskills section

--- portfoliosite/src/App.jsx
+++ portfoliosite/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import DotGroup from './scenes/DotGroup';
 import Landing from './scenes/Landing';
+import MySkills from './scenes/MySkills';
 import LineGradient from './components/LineGradient'
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from './scenes/Navbar'
@@ -40,6 +41,10 @@ function App() {
       <Landing setSelectedPage={setSelectedPage} />
     </div>
     <LineGradient />
+   <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+   </div>
+   
    </div>
   )
 }
