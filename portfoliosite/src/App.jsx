ef363e93584e77263bca1a5ba9dcece60b0df6ff
Travis <travisjb0@gmail.com>
2023-05-18 19:07:20 -0700

    added complete navbar

--- portfoliosite/src/App.jsx
+++ portfoliosite/src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import DotGroup from './scenes/DotGroup';
+import Landing from './scenes/Landing';
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from './scenes/Navbar'
 
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home')
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTipOfPage(true);
+      if (window.scrollY !== 0) setIsTipOfPage(false);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  }, []);
 
   return (
    <div className="app bg-deep-blue">
     <Navbar 
-      selectedPage ={selectedPage} setSelectedPage={setSelectedPage}
+    isTopOfPage={isTopOfPage}   
+    selectedPage ={selectedPage} 
+    setSelectedPage={setSelectedPage}
     /> 
+    <div className="w-5/6 mx-auto md:h-full">
+      {isAboveMediumScreens && (
+        <DotGroup
+          selectedPage = {selectedPage}
+          setSelectedPage = {setSelectedPage}
+        />
+      )}
+      <Landing setSelectedPage={setSelectedPage} />
+    </div>
    </div>
   )
 }
---
+++ portfoliosite/src/scenes/Landing.jsx
@@ -0,0 +1,16 @@
+import useMediaQuery from "../hooks/useMediaQuery";
+import {motion} from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+const Landing = ({setSelectedPage}) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+    return (
+        <section id="home"> 
+    
+        </section>
+    )
+} 
+
+export default Landing;
