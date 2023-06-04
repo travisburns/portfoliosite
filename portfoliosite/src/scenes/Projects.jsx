1d0e270d73187da6bbc763d238cff38c5ca7f7c9
Travis <travisjb0@gmail.com>
2023-06-03 19:37:18 -0700

    Added project title and description for screen size 320 width

--- portfoliosite/src/scenes/Projects.jsx
+++ portfoliosite/src/scenes/Projects.jsx
@@ -2,13 +2,19 @@ import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 import Project1 from "../assets/project-1.jpeg"
 import projectArrow from "../assets/project-arrow.png"
-
+import github from "../assets/github.png"
 
 
 const Projects = () => {
   return (
     
     <section id="projects" className="pt-48 pb-48">
+      <div className="projects-title w-[90vw] ml-[5vw] mr-[5vw] pb-[12rem]">
+      <p className="project-title_desc text-[2.7em] w-[100%] font-playfair mb-5">Flawlessly Fusing Creativity and Expertise</p>
+      <div className="project-title-para">
+        <p className="project-title-para-text w-[262px] h-[35px] text-[1.1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint minima consequatur, eius nobis odit accusantium.</p>
+      </div>
+      </div>
       
       <div className="item-container w-[100%]">
         <div className="item-info">
@@ -26,6 +32,9 @@ const Projects = () => {
               </div>
               <div className="project-arrow relative top-[7rem] left-[2rem]">
                 <img src={projectArrow} alt="" className="absolute right-[30px] bottom-[34px] w-[1em] h-[1em]"/>
+              </div>
+              <div className="project-github">
+              <img src={github} alt="" className=" relative top-[25px] left-[13rem] w-[1.5em] h-[1.5em]"/>
               </div>
               </div>
           </div>
