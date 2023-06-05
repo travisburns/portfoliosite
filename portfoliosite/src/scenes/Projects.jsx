293bb0aab5f7fa6fca6450cd54da048d78dbaf4f
Travis <travisjb0@gmail.com>
2023-06-05 15:47:43 -0700

    Added better project section stats that are responsive on each size, however, due to the overall width of the canvas. had to adjust canvas which changed the project stats. Need to adjust again

--- portfoliosite/src/scenes/Projects.jsx
+++ portfoliosite/src/scenes/Projects.jsx
@@ -18,29 +18,30 @@ const Projects = () => {
       </div>
       </div>
       {/* {Project container} */}
-      <div className="item-container w-[100%] text-[.65em] ml-[1.5rem] sm:ml-[4rem] md:ml-[5.5rem] lg:ml-[6.5rem]">
+      <div className="item-container relative  w-[82.5%] text-[.65em] ml-[1.5rem] sm:ml-[4rem] md:ml-[5.5rem] lg:ml-[8.2rem] xxl:ml-[10rem] ">
         {/* Project info */}
         <div className="item-info">
           {/* Project image */}
           <div className="item-image-container relative">
-            <div className="item-img h-[150px] xs:h-[200px] sm:h-[250px] relative">
+            <div className="item-img  h-[150px] xs:h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px] xxl:h-[380px] relative">
               <img src={Project1} className="featured-item-image-inner relative w-[70%] h-[100%]"/>
               </div>
               {/* Project info/ turns into right hand box at 860 width */}
-              <div className="item-context p-[20px] relative bottom-[5rem] sm:bg-black sm:p-[20px] sm:w-[15%] sm:h-[100px] sm:top[-3rem] sm:left-[30rem]">
+              <div className="item-context p-[20px] relative bottom-[5rem] sm:bg-black xs:bottom-[10rem] sm:p-[20px] sm:w-[15%] sm:h-[100px] sm:bottom-[4rem] sm:left-[60%]">
               {/* Above is the parent container, likely will need to be overaching stats on this above and take them off the below if possible */}
-              <div className="item-desc relative m-[0 0 2.5em] text-[0.5rem] leading-[1.3em] text-left font-playfair text-black bottom-[7rem] xs:bottom-[2rem] sm:text-white sm:bottom-[0.8rem]">
+              <div className="item-desc relative m-[0 0 2.5em] text-[0.5rem] leading-[1.3em] text-left font-playfair text-black bottom-[4rem] xs:bottom-[2rem] sm:text-white sm:bottom-[0.8rem]">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
               </div>
      
-              <div className="item-title relative m-[0 0 2.5em] top-[0rem] text-[0.5rem] leading-[1.3em] text-left font-Lato text-black sm:text-white sm:top-[1rem]">
+              <div className="item-title flex m-[0 0 2.5em] w-[70%] mt-[1rem] text-[0.5rem] leading-[1.3em] text-left font-Lato xs:mt-[6rem] text-black sm:text-white sm:mt-[0] sm:w-[100%]">
                 <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div className="project-links relative top-[7rem] xs:left-[5rem] sm:flex sm:top-[1rem] sm:left-[3.5rem]">
-              <img src={github} alt="" className="  bottom-[60px] w-[2em] h-[2em] sm:top-[-0.5rem] relative" />
-                <img src={projectArrow} alt="" className=" bottom-[34px] w-[1em] h-[1em] "/>
+                <div className="project-links flex relative mt-[0.3rem] ml-[40%] w-[20%] min-w-[20%] xs:left-[20%] xs:w-[10%] sm:block sm:top-[0rem] sm:left-[1rem]">
+                <img src={github} alt="" className=" bottom-[0.40rem]  w-[3em] h-[3em] sm:top-[-0.5rem] relative" />
+                <img src={projectArrow} alt="" className=" w-[2em] h-[2em] "/>
                 
               </div>
+              </div>
+              
              
               </div>
           </div>
